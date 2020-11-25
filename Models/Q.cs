d2ea3c8ba0bfdff9cc40a3c0571a2f5d1623da8a
LiamBartolini <liambartolini@gmail.com>
2020-11-25 23:14:41 +0100

    clear code

--- Models/Q.cs
+++ Models/Q.cs
@@ -79,20 +79,13 @@ namespace Bartolini.Liam._4H.ClasseQuadrato.Models
         {
             Q qInt = new Q();
 
-            //qInt.Lato = q2.Lato - (q1.PosX + q1.Lato);
-            //int altezza = q1.Lato - (q1.PosY - q2.PosY);
-
-            // Se il secondo quadrato è completmente dentro il primo ritorno il secondo quadrato
+            // Controllo se uno dei due quadrati è completamente integrato all'altro
             if (q2.PosX + q2.Lato >= q1.PosX && q2.PosX + q2.Lato <= q1.PosX + q1.Lato && q2.PosY >= q1.PosY - q1.Lato && q2.PosY <= q1.PosY)
                 return q2;
             
             if (q1.PosX + q1.Lato >= q2.PosX && q1.PosX + q1.Lato <= q2.PosX + q2.Lato && q1.PosY >= q2.PosY - q2.Lato && q1.PosY <= q2.PosY)
                 return q1;
 
-            //qInt.Lato = q1.Lato - (q2.PosX - q1.PosX);
-            //int altezza = q2.Lato - (q1.PosY - q2.PosY);
-
-            //qInt.Area = altezza;
             // Controllo se il secondo quadrato interseca il primo
             if (q2.PosX > q1.PosX + q1.Lato && q2.PosX < q1.PosX && q2.PosY > q1.PosY - q1.Lato && q2.PosY < q1.PosY)
             {
