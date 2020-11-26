039a3e30a30d2d7aff35bbbdb401f0fd46dc3457
LiamBartolini <liambartolini@gmail.com>
2020-11-26 08:19:49 +0100

    ##

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@ namespace Bartolini.Liam._4H.ClasseQuadrato
     {
         static void Main(string[] args)
         {
-            Q q1 = new Q(2, 1, 2);
-            Q q2 = new Q(4, 1, 2);
+            Q q1 = new Q(4, -2, 1);
+            Q q2 = new Q(7, 4, 2);
 
             Console.WriteLine("QUADRATO 1: ");
-            Console.WriteLine($"{q1.Area:n2}\tcm^2 \n{q1.Perimetro:n2}\tcm \n{q1.Diagonale:n2}\tcm");
+            Console.WriteLine($"Area: \t\t{q1.Area:n2}\tcm^2 \nPerimetro: \t{q1.Perimetro:n2}\tcm \nDiagonale: \t{q1.Diagonale:n2}\tcm");
 
             Console.WriteLine("\nQUADRATO 2: ");
-            Console.WriteLine($"{q2.Area:n2}\tcm^2 \n{q2.Diagonale:n2}\tcm \n{q2.Diagonale:n2}\tcm");
+            Console.WriteLine($"Area: \t\t{q2.Area:n2}\tcm^2 \nPerimetro: \t{q2.Perimetro:n2}\tcm \nDiagonale: \t{q2.Diagonale:n2}\tcm");
 
             Console.Write("\nQuadrato 1 < Quadrato 2:");
             Console.Write($"\t{q1 < q2}");
